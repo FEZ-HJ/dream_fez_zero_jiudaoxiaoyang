4c672591703fc9c044d3677b8da3ca63f8d74ce5
FEZ-HJ <fez-hj@163.com>
2018-09-18 23:57:12 +0800

    like组件初步完成

--- components/like/index.js
+++ components/like/index.js
@@ -1,3 +1,33 @@
 Component({
+  properties:{
+    like:{
+      type: Boolean,
+      value:false,
+      observer:function(){
 
+      }
+    },
+    count:{
+      type:Number
+    }
+
+  },
+
+  data:{
+    yesSrc: '../images/like.png',
+    noSrc: '../images/like@dis.png',
+  },
+
+  methods:{
+    onLike:function(event){
+      let like = !this.properties.like
+      let count = this.properties.count
+
+      count  = like ? ++count : --count
+      this.setData({
+        count: count,
+        like: like
+      })
+    }
+  }
 })
