2ac9886e675162c87c5abc4c266d1980fc29ff5b
FEZ-HJ <fez_hj@163.com>
2018-09-23 23:05:18 +0800

    基本功能实现

--- components/classic/music/music.js
+++ components/classic/music/music.js
@@ -1,5 +1,7 @@
 import { classicBeh } from '../classic-beh.js'
 
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
@@ -13,14 +15,30 @@ Component({
    * 组件的初始数据
    */
   data: {
+    playing: false,
     pauseSrc: 'images/player@waitting.png',
     playSrc: 'images/player@playing.png'
   },
 
+  detached:function(event){
+    mMgr.stop()
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onPlay: function(event){
+      if (!this.data.playing){
+        this.setData({
+          playing: !this.data.playing
+        })
+        mMgr.src = this.properties.src
+      }else{
+        this.setData({
+          playing: !this.data.playing
+        })
+        mMgr.pause()
+      }
+    }
   }
 })
